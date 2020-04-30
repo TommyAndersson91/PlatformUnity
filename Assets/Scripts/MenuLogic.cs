4a49e05f70409fa95251a0271a00b33406ac2a81
TommyAndersson1991 <tommy.andersson@iths.se>
2020-04-30 14:43:49 +0700

    Added saving and loading of progress. Added new Enemy type and levels.

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -16,15 +16,27 @@ public class MenuLogic : MonoBehaviour
   {
     PlayLevelOne.onClick.AddListener(PlayLevel);
     PlayLevelTwo.onClick.AddListener(PlayLevel);
+    PlayLevelTwo.gameObject.SetActive(false);
+    PlayerData data = SaveSystem.LoadPlayer(); 
+    if (data != null)
+    {
+      Debug.Log("The Level at menu is " + data.level);
+      Debug.Log("The Lives at menu is " + data.health);
+      if (data.level > 1)
+      {
+      PlayLevelTwo.gameObject.SetActive(true);
+      }
+    }
+      
+    
   }
+
 
   private void PlayLevel()
   {
 
     SceneManager.LoadScene(EventSystem.current.currentSelectedGameObject.name);
-  }
-
- 
+  } 
 }
 
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-  private Rigidbody2D rb;
+  public Rigidbody2D rb;
   private Animator playerAnimator;
   private enum State { idle, running, jumping, falling, hurt }
   private State state = State.idle;
   private Collider2D coll;
   private bool colliding = false;
   private bool isHurt = false;
-  private delegate void UpdateCherries(int value);
-  private UpdateCherries onUpdateCherries;
+  public int level;
+  private bool isCollided = false;
 
   private delegate void UpdateLives(int value);
   private UpdateLives OnUpdateLives;
+
+  private delegate void UpdateCherries(int value);
+  private UpdateCherries onUpdateCherries;
 
   [SerializeField] private LayerMask ground;
   [SerializeField] private float speed = 7f;
@@ -52,42 +55,71 @@ public class PlayerController : MonoBehaviour
     }
   }
 
+  private void Awake()
+  {
+    onUpdateCherries += UpdateCherryText;
+    OnUpdateLives += UpdateHeartsGrid;
+    LoadPlayer();
+  }
+
   private void Start()
   {
     rb = GetComponent<Rigidbody2D>();
     playerAnimator = GetComponent<Animator>();
     coll = GetComponent<Collider2D>();
-    onUpdateCherries += UpdateCherryText;
-    OnUpdateLives += UpdateHeartsGrid;
-    for (int i = 0; i < Lives; i++)
-    {
-      GameObject heart = Instantiate(Heart ,new Vector3(0, 0, 10), Quaternion.identity) as GameObject;
-      heart.transform.SetParent(HeartsHolder);
-    }
+  }
 
+  public void SavePlayer()
+  {
+    SaveSystem.SavePlayer(this);
+  }
+
+  public void LoadPlayer()
+  {
+    PlayerData data = SaveSystem.LoadPlayer();
+
+    level = data.level;
+    Lives = data.health;
   }
 
   private void UpdateHeartsGrid(int value)
   {
-    if (value == 0)
+    if (value == 0 && SceneManager.GetActiveScene().buildIndex != 0)
     {
+      Lives = 3;
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    if (HeartsHolder.childCount > value && value > 0)
+    if (HeartsHolder.childCount > value && value > 0 && SceneManager.GetActiveScene().buildIndex != 0)
     {
       Destroy(HeartsHolder.GetChild(HeartsHolder.childCount - 1).gameObject);
     }
-    else if (transform.childCount < value)
+    else if (HeartsHolder.childCount < value && SceneManager.GetActiveScene().buildIndex != 0)
     {
-      GameObject heart = Instantiate(Heart, new Vector3(0, 0, 10), Quaternion.identity) as GameObject;
-      heart.transform.SetParent(HeartsHolder);
+      for (int i = 0; i < value; i++)
+      {
+        if (HeartsHolder.childCount == value)
+        {
+          return;
+        }
+        GameObject heart = Instantiate(Heart, new Vector3(0, 0, 10), Quaternion.identity) as GameObject;
+        heart.transform.SetParent(HeartsHolder);
+      }
+
     }
 
   }
 
   private void UpdateCherryText(int value)
   {
-    cherryText.text = value.ToString();
+    if (value < 5)
+    {
+      cherryText.text = value.ToString();
+    }
+    else
+    {
+      Lives += 1;
+      Cherries = 0;
+    }
   }
 
   private void Update()
@@ -110,7 +142,7 @@ public class PlayerController : MonoBehaviour
     if (other.gameObject.tag == "Enemy")
     {
 
-      if (state == State.falling && coll.bounds.min.y > other.rigidbody.GetComponent<Collider2D>().bounds.max.y)
+      if (state == State.falling && coll.bounds.min.y > other.rigidbody.GetComponent<Collider2D>().bounds.center.y)
       {
         Destroy(other.gameObject);
       }
@@ -136,9 +168,15 @@ public class PlayerController : MonoBehaviour
     }
     else if (other.gameObject.tag == "House")
     {
-      if (SceneManager.GetSceneByBuildIndex(SceneManager.GetActiveScene().buildIndex+1) != null)
+      if (level < SceneManager.GetActiveScene().buildIndex + 1)
       {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        level = SceneManager.GetActiveScene().buildIndex + 1;
+        Debug.Log("Level saved as " + level);
+        SavePlayer();
+      }
+      if (SceneManager.GetSceneByBuildIndex(SceneManager.GetActiveScene().buildIndex + 1) != null)
+      {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
       }
     }
   }
@@ -158,16 +196,61 @@ public class PlayerController : MonoBehaviour
 
   private void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.tag == "Collectible")
+    if (!isCollided)
     {
-      Destroy(other.gameObject);
-      Cherries += 1;
+      isCollided = true;
+      if (other.tag == "Collectible")
+      {
+        Destroy(other.gameObject);
+        Cherries += 1;
+      }
+      else if (other.gameObject.tag == "Shrooms" && Lives < 10)
+      {
+        Destroy(other.gameObject);
+        Lives += 1;
+      }
+      else if (other.gameObject.tag == "Crank")
+      {
+        if (GameObject.Find("Block"))
+        {
+          Sprite sp = Resources.Load<Sprite>("Sunnyland/artwork/Environment/props/crank-up");
+          other.gameObject.GetComponent<SpriteRenderer>().sprite = sp;
+          Destroy(GameObject.Find("Block").gameObject);
+        }
+      }
+      else if (other.gameObject.tag == "Spikes")
+      {
+        state = State.hurt;
+        Lives -= 1;
+        colliding = true;
+        if (other.gameObject.transform.position.x > transform.position.x)
+        {
+          // Enemy is to my right. Therefore i should be damaged and move left
+          rb.velocity = new Vector2(-hurtForce, hurtForce);
+          transform.localScale = new Vector2(-1, 1);
+
+        }
+        else
+        {
+          //Enemy is to my left. Therefore i should be damaged and move right
+          rb.velocity = new Vector2(hurtForce, hurtForce);
+          transform.localScale = new Vector2(1, 1);
+        }
+      }
+      StartCoroutine(TriggerWait());
     }
-    else if (other.gameObject.tag == "Shrooms" && Lives < 3)
-    {
-      Lives++;
-      Destroy(other.gameObject);
-    }
+  }
+
+  private void OnTriggerExit2D(Collider2D other)
+  {
+
+  }
+
+  private IEnumerator TriggerWait()
+  {
+    yield return new WaitForSeconds(0.5f);
+    isCollided = false;
+    colliding = false;
   }
 
   private void Movement()
@@ -220,5 +303,10 @@ public class PlayerController : MonoBehaviour
     {
       state = State.idle;
     }
+  }
+
+  private void OnDestroy()
+  {
+    SavePlayer();
   }
 }
